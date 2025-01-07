48facf7b8afd15c47639d7a288a41a8a97284437
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-01-06 18:58:26 -0600

    Breaking ground in javascript

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-//Grid Creation
+
 const playGame = () => {
+  //Grid Creation
+  const grid = [];
     const makeGrid = () => {
-      const grid = [];
+      
       for (let i = 0; i < 9; i++) {
-        grid.push(i + 1);
+        grid.push(null);
       }
       return grid;
-    }
-    return { makeGrid };
+    };    
+    //matches users coordinates with the index
+    const logInputX = (input) => {
+      //check if input exists 
+      if (grid[input] === null) {
+        grid[input] = 'X';
+      }
+      else {
+        alert("You must chose somewhere not picked already.");
+      };
+      return grid;
+    };
+    const logInputO = (input) => {
+      //check if input exists 
+      if (grid[input] === null) {
+        grid[input] = 'O';
+      }
+      else {
+        alert("You must chose somewhere not picked already.");
+      };
+      return grid;
+
+    };
+    
+
+    return { makeGrid, logInputX, logInputO };
   }
+
+ //to make grid, do const grid = playGame() -- then do grid.makeGrid() to call the object(function) on the new variable to  access it. otherwise will not work.
+ const grid = playGame()
+ console.log(grid.makeGrid());
+ console.log(grid.logInputX(2));
