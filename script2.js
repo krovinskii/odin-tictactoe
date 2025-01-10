4daaa9445fe16e5577d615487175c23a5bf7dcc1
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-01-09 19:00:09 -0600

    Logic for starting player

--- script2.js
+++ script2.js
@@ -1,20 +1,64 @@
 const playerInformation = () => {
+  let playerAssignments = {
+    player1: "",
+    player2: "",
+  };
   //First we start off with making the player chose if they would like X or O
   const makePlayerXO = (userInputForXO) => {
-    let player1 = "";
-    let player2 = "";
-    userInputForXO.toLowerCase();
-    if (userInputForXO === "x") {
-      player1 = "x";
-      player2 = "o";
-    } else if (userInputForXO === "o") {
-      player1 = "o";
-      player2 = "x";
+    if (userInputForXO.toLowerCase() === "x") {
+      playerAssignments.player1 = "x";
+      playerAssignments.player2 = "o";
+    } else if (userInputForXO.toLowerCase() === "o") {
+      playerAssignments.player1 = "o";
+      playerAssignments.player2 = "x";
     } else {
       const userInputWrong = "Please enter X or O";
       console.log(userInputWrong);
+      return;
     }
-    return { player1, player2 };
+    return playerAssignments;
   };
-  return { makePlayerXO };
+
+  //Then we ask player 1 to chose heads or tails, assign to different key in same object
+  const makePlayerHeadsOrTails = (userInputForHeadsOrTails) => {
+    console.log(`Player 1 is ${playerAssignments.player1}`);
+    console.log(`Player 2 is ${playerAssignments.player2}`);
+    if (userInputForHeadsOrTails.toLowerCase() === "heads") {
+      playerAssignments.player1Coin = "heads";
+      playerAssignments.player2Coin = "tails";
+    } else if (userInputForHeadsOrTails.toLowerCase() === "tails") {
+      playerAssignments.player1Coin = "tails";
+      playerAssignments.player2Coin = "heads";
+    }
+    return playerAssignments;
+  };
+  //Then we do the coin flip
+  const coinFlip = () => {
+    let coinFlipResult = Math.floor(Math.random() * 10);
+    if (coinFlipResult < 5) {
+      coinFlipResult = true;
+      console.log("Coin was tails. Under 5");
+      return coinFlipResult;
+    } else {
+      coinFlipResult = false;
+      console.log("Coin was heads. Over 5");
+      return coinFlipResult;
+    }
+  };
+
+  //Finally we compare the coin flip results to what the players chose to determine starting player.
+  const startingPlayer = () => {
+    const coinFlipResult = coinFlip();
+    if (
+      (playerAssignments.player1Coin === "heads" && !coinFlipResult) ||
+      (playerAssignments.player1Coin === "tails" && coinFlipResult)
+    ) {
+      console.log("Player 1 has won the coin flip. Player 1 starts!");
+      return "player1";
+    } else {
+      console.log("Player 2 has won the coin flip. Player 2 starts!");
+      return "player2";
+    }
+  };
+  return { makePlayerXO, makePlayerHeadsOrTails, coinFlip, startingPlayer };
 };
